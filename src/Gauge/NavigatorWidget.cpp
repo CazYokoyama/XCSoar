507c6c7e744081ceecef9cb895948a145788e0b3
Caz Yokoyama <caz@caztech.com>
2025-02-26 14:55:40 +0900

    Show waypoint/turn point icons on far left and right.
    Signed-off-by: Caz Yokoyama <caz@caztech.com>

--- src/Gauge/NavigatorWidget.cpp
+++ src/Gauge/NavigatorWidget.cpp
@@ -59,6 +59,8 @@ NavigatorWindow::OnPaint(Canvas &canvas) noexcept
   WaypointPtr wp_before;
   WaypointPtr wp_current;
 
+  unsigned task_size{};
+
   auto *protected_task_manager =
       backend_components->protected_task_manager.get();
   if (protected_task_manager != nullptr) {
@@ -71,6 +73,8 @@ NavigatorWindow::OnPaint(Canvas &canvas) noexcept
       if (tp == TaskType::ORDERED) {
 
         const OrderedTask &task = lease->GetOrderedTask();
+
+        task_size = task.TaskSize();
 
         wp_current = task.GetActiveTaskPoint()->GetWaypointPtr();
 
@@ -99,6 +103,9 @@ NavigatorWindow::OnPaint(Canvas &canvas) noexcept
     NavigatorRenderer::DrawProgressTask(
         CommonInterface::Calculated().common_stats.ordered_summary, canvas,
         canvas.GetRect(), look_nav, look_task);
+
+  NavigatorRenderer::DrawWaypointsIconsTitle(canvas, wp_before, wp_current,
+                                             task_size, look_nav);
 }
 
 void
